b0fc265d20e63f6b003ff26a3188f5cb577123c3
Harine K S (CI XSE5) <KS.Harine@in.bosch.com>
2022-12-02 04:09:19 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Dinesh
+          Welcome to Bosch
         </a>
       </header>
     </div>
