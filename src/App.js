47db515dbc575812494ddc639785ed37807c9948
Harine K S (CI XSE5) <KS.Harine@in.bosch.com>
2022-12-02 04:41:37 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to BGSW
+          Welcome to BGSW 25
         </a>
       </header>
     </div>
